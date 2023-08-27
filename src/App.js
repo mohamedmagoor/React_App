71d01ec99d2d49e810275e01de3b36dc1c843ca1
Mohamed Magoor <mohamedmagoor6@gmail.com>
2023-08-27 11:07:54 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+export default function nav(){
+    return<>
+     <div className="container">
+       
+         <h1>Mohamed Magoor</h1>
+    <h2>Web Develper & Designer</h2>
+    
+         <button  className="button">Contact me</button>
+       </div>
+    </>
+}
